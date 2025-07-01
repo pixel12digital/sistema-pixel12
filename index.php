0c874e555ac94276c7866885d0c9d34906998fd8
Seu Nome <seuemail@gmail.com>
2025-07-01 13:45:18 -0300

    teste

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Carregar configurações
 require_once 'config.php';
 require_once 'src/db.php';
