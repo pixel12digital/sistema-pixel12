b2446b43adfa4d1c7f4788cd1d404c7416513ba0
Seu Nome <seuemail@gmail.com>
2025-07-01 14:11:01 -0300

    Permite versionar config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+// Configurações globais do sistema
+// Exemplo: dados do admin, host padrão, etc.
+define('ADMIN_USER', 'admin');
+define('ADMIN_PASS', 'admin123');
+
+// Configuração do banco de dados remoto Hostinger
+// Utilize sempre o mesmo banco para local e produção
+
+define('DB_HOST', 'srv1607.hstgr.io'); // ou '193.203.166.216'
+define('DB_NAME', 'u342734079_revendaweb');
+define('DB_USER', 'u342734079_revendaweb');
+define('DB_PASS', 'Los@ngo#081081');
+
+// Configuração da API do Asaas
+define('ASAAS_API_KEY', '$aact_prod_000MzkwODA2MWY2OGM3MWRlMDU2NWM3MzJlNzZmNGZhZGY6OjQ5NTYyZTZiLTJmMzMtNDk2NS04ZjJiLWVlNzJiNTYzY2JiNDo6JGFhY2hfOTY3MTVhN2MtMjkyMS00YTgwLWIyOTAtNThiMzQyOTljYjQ4');
+define('ASAAS_API_URL', 'https://www.asaas.com/api/v3');
+?> 
